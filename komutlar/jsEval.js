1529ec4a3789e096e337672b04e843f4f015f1f3
Falsis <63756985+falsisdev@users.noreply.github.com>
2021-07-18 02:01:28 +0300

    edit: Edited JavaScript Evalution Code

--- komutlar/jsEval.js
+++ komutlar/jsEval.js
@@ -1,11 +1,20 @@
 module.exports = ({
 name:"js",
 bkz:"JavaScript Eval Komutu",
-code:`$djsEval[const falsisdb = require("falsisdb")
-const db = new falsisdb()
+code:`$djsEval[
 const path = require("path")
+function file(file){
+if(!file){
+return "Bir dosya yaz"
+}
+return require(path.join(process.cwd(), file))
+}
+const falsisdb = require("falsisdb")
+const db = new falsisdb()
 const fs = require("fs")
 const fetch = require("node-fetch")
+const color = require(path.join(process.cwd(), "color.json"))
 $message;yes]
-$onlyForIDs[$botOwnerID;700763255978852462;606828535423959041;]`
+$onlyForIDs[$botOwnerID;700763255978852462;606828535423959041;]
+`
 })
