b9e989e6b783c753de5219aa9f79c614a23cc6fb
Falsis <63756985+falsisdev@users.noreply.github.com>
2021-08-14 18:13:22 +0300

    feat(command): added text to binary command

---
+++ komutlar/binary.js
@@ -0,0 +1,17 @@
+module.exports = {
+name: 'binary',
+bkz: 'Binary - Yazı çevirisi',
+code:`$title[Binary Yazı Çeviricisi]
+$addField[Binary Kodu:;\`\`\`binary
+$djsEval[var output = ""
+var input = "merhaba"
+for (var i = 0; i < input.length; i++) { 
+output += input[i].charCodeAt(0).toString(2) + " "; 
+};yes]\`\`\`]
+$addField[Yazı:;\`\`\`txt
+$noMentionMessage\`\`\`]
+$color[BDAAF9]
+$thumbnail[$authorAvatar]
+$footer[Kremlin is Happy to Support Developers...]
+$onlyIf[$noMentionMessage!=;❌ Lütfen bir metin girin.]`
+}
