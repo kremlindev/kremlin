d2a1cacbcf9e9f5b631e75db1fadb3414924cfbe
Falsis <63756985+falsisdev@users.noreply.github.com>
2021-08-14 18:14:54 +0300

    fix(binary): fixed binary command input

--- komutlar/binary.js
+++ komutlar/binary.js
@@ -4,7 +4,7 @@ bkz: 'Binary - Yazı çevirisi',
 code:`$title[Binary Yazı Çeviricisi]
 $addField[Binary Kodu:;\`\`\`binary
 $djsEval[var output = ""
-var input = "merhaba"
+var input = $noMentionMessage
 for (var i = 0; i < input.length; i++) { 
 output += input[i].charCodeAt(0).toString(2) + " "; 
 };yes]\`\`\`]
