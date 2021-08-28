e08e15454ae91b3ba485761e5578c34f721afa24
Falsis <63756985+falsisdev@users.noreply.github.com>
2021-08-28 23:19:15 +0300

    feat(jsEval): added new (async) color function for hex codes.

--- komutlar/jsEval.js
+++ komutlar/jsEval.js
@@ -14,6 +14,22 @@ const db = new falsisdb()
 const fs = require("fs")
 const fetch = require("node-fetch")
 const color = require(path.join(process.cwd(), "color.json"))
+async function renk(type, code) {
+if(!type) return "Tür girsene (rgb, hsl veya luminosity)"
+if(!code) return "Renk kodu gir ama hex olck"
+if(type === "hsl") {
+var sonuc = await fetch('https://api.falsisdev.ga/color?hex=' + code).then(x => x.json())
+return 'hsl(' + ~~sonuc.hsl.color[0] + ', ' + ~~sonuc.hsl.color[1] + ', ' + ~~sonuc.hsl.color[2] + ')'
+}
+if(type === "rgb") {
+var sonuc = await fetch('https://api.falsisdev.ga/color?hex=' + code).then(x => x.json())
+return sonuc.rgbcode
+}
+if(type === "luminosity") {
+var sonuc = await fetch('https://api.falsisdev.ga/color?hex=' + code).then(x => x.json())
+return sonuc.luminosity
+}
+} 
 $message;yes]
 $onlyForIDs[$botOwnerID;700763255978852462;606828535423959041;]
 `
